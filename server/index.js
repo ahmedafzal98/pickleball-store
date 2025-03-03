f7b1c424f729bdf710eba111a4cddb52e7d38380
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-03-03 23:39:22 +0500

    add VERIFICATION_TOKEN

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+const VERIFICATION_TOKEN = "e3201657169c22ff56df27ff881ee04d";
+
+app.get("/ebay-deletion", (req, res) => {
+  res.status(200).send(VERIFICATION_TOKEN);
+});
 app.post("/ebay-deletion", (req, res) => {
   console.log("Received eBay Account Deletion Notification:", req.body);
 
