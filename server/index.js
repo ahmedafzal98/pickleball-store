c194fe30794cf79b26ebea0645e846f22668f48d
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-03-03 23:32:48 +0500

    add ebay-deletion endpoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.use(express.json());
+
+app.post("/ebay-deletion", (req, res) => {
+  console.log("Received eBay Account Deletion Notification:", req.body);
+
+  res.status(200).send("Received");
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log("Server running on port 3000"));
